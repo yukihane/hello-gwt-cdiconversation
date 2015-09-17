228944b799579d171117dd90f2fd2fd804ac90f8
yukihane <yukihane.feather@gmail.com>
2015-09-17 11:25:10 +0900

    formatting

--- src/main/java/com/example/client/Messages.java
+++ src/main/java/com/example/client/Messages.java
@@ -1,10 +1,10 @@
 package com.example.client;
 
 public interface Messages extends com.google.gwt.i18n.client.Messages {
-  
-  @DefaultMessage("Enter your name")
-  String nameField();
 
-  @DefaultMessage("Send")
-  String sendButton();
+    @DefaultMessage("Enter your name")
+    String nameField();
+
+    @DefaultMessage("Send")
+    String sendButton();
 }
